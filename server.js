ef8b83d6466f2df302df0c78b3f3547640d0165c
Craig McGowan <craigmcgowan15@gmail.com>
2021-09-14 21:22:31 -0400

    index page created

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express")
-const app = express()
+const app = express();
+const drinks = require("./models/drinks");
 
 app.listen(3000, ()=>{console.log('Kegs are tapped...')})
 
 app.get("/", (req, res) => {
     res.send('Welcome to the Gitpub App!')
+})
+
+app.get("/drinks", (req, res) =>{
+    res.send(drinks)
 })
